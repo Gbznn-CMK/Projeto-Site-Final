083d46c73cd88839d92d490b1284857253658d07
Gabriel Ferreira Freire <gabrielferreirafreire004gmail.com>
2026-09-03 16:45:44 -0300

    Começo

---
+++ Projeto-Site-Final/src/app/Core/Facade/facde.services.ts
@@ -0,0 +1,5 @@
+import { Injectable , inject } from "@angular/core";
+
+export class perfilUsuario{
+    
+}
---
+++ Projeto-Site-Final/src/app/Features/perfil-user/perfil-user.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-user',
+  imports: [],
+  templateUrl: './perfil-user.html',
+  styleUrl: './perfil-user.css',
+})
+export class PerfilUser {}
