d21ddeb0b7f5d013f158d8cd599f85fc9d180c5e
Gabriel Ferreira Freire <gabrielferreirafreire004gmail.com>
2026-09-04 16:25:03 -0300

    Precisa mexer na foto

--- Projeto-Site-Final/src/app/app.ts
+++ Projeto-Site-Final/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PerfilUser } from "./Features/perfil-user/perfil-user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PerfilUser],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
