22b4bb9a9030e4697a4acb539638c5072c84ca7a
Gabriel Ferreira Freire <gabrielferreirafreire004gmail.com>
2026-09-08 16:45:11 -0300

    Necessario mecher na imagem , e box

--- Projeto-Site-Final/src/app/app.routes.ts
+++ Projeto-Site-Final/src/app/app.routes.ts
@@ -5,7 +5,15 @@ export const routes: Routes = [
         path:'',
         loadComponent:() =>
             import ('./Features/buscar/buscar') .then((m)=>m.Buscar)
-
-
+    },
+     {
+        path:'Buscar',
+        loadComponent:() =>
+            import ('./Features/buscar/buscar') .then((m)=>m.Buscar)
+    },
+    {
+        path:'Perfil',
+        loadComponent :() =>
+        import ('./Features/perfil-user/perfil-user').then((m)=>m.PerfilUser)
     }
 ];
--- Projeto-Site-Final/src/app/app.ts
+++ Projeto-Site-Final/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PerfilUser } from "./Features/perfil-user/perfil-user";
+import { Buscar } from "./Features/buscar/buscar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PerfilUser],
+  imports: [RouterOutlet, PerfilUser, Buscar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
